47d336266d0d7f5c510bb86d7c8490703377159f
Pokkrong P <34023245+Nevioxis@users.noreply.github.com>
2023-09-12 11:56:16 +0700

    Update index.js

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,32 +1,17 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cote = require('cote')
-const axios = require('axios')
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 3000; // Define your desired port number
 
-const app = express()
-
+// Middleware setup, if needed
 app.use(bodyParser.json())
-
-const restaurantsRequester = new cote.Requester({ name: 'restaurants requester', key: 'restaurants' })
-
-const orderRequester = new cote.Requester({ name: 'order requester', key: 'orders' })
-
-const deliveryRequester = new cote.Requester({ name: 'delivery requester', key: 'deliveries' })
-
+// Define your route
 app.get('/', (req, res) => {
-    res.send({"status":"ready"});
-})
+    res.send({ "status": "ready" });
+});
 
-app.get('/restaurants', async (req, res) => {
-    const restaurants = await restaurantsRequester.send({ type: 'list' })
-    res.send(restaurants);
-})
+// Start the server
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
 
-app.post('/order', async (req, res) => {
-    const order = await orderRequester.send({ type: 'create order', order: req.body })
-    const delivery = await deliveryRequester.send({ type: 'create delivery', order })
-
-    res.send({ order, delivery })
-})
-
-app.listen(3000, () => console.log('listening'))
